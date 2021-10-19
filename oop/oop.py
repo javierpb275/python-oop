45130493e4023f0540712a71081e8beea90313a1
javierpb275 <javierpb275@gmail.com>
2021-10-19 19:08:21 +0200

    first commit

---
+++ oop/oop.py
@@ -0,0 +1,20 @@
+# OOP (Object Oriented Programming)
+print(type(None))  # <class 'NoneType'>
+print(type(True))  # <class 'bool'>
+print(type(5))  # <class 'int'>
+print(type(5.5))  # <class 'float'>
+print(type('hi'))  # <class 'str'>
+print(type([]))  # <class 'list'>
+print(type(()))  # <class 'tuple'>
+print(type({}))  # <class 'dict'>
+
+'''
+<class 'NoneType'>
+<class 'bool'> 
+<class 'int'>  
+<class 'float'>
+<class 'str'>
+<class 'list'>
+<class 'tuple'>
+<class 'dict'>
+'''
