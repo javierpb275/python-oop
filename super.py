fa99dff527d5c6eeedb4f4ae70cef8a9a0b43f2c
javierpb275 <javierpb275@gmail.com>
2021-10-24 23:42:00 +0200

    created super.py file

---
+++ super.py
@@ -0,0 +1,29 @@
+class User():
+    def sign_in(self):
+        print('logged in')
+
+    def attack(self):
+        print('do nothing')
+
+
+class Wizard(User):
+    def __init__(self, name, power):
+        self.name = name
+        self.power = power
+
+    def attack(self):
+        User.attack(self)
+        print(f'attack with power of {self.power}')
+
+
+class Archer(User):
+    def __init__(self, name, arrows):
+        self.name = name
+        self.arrows = arrows
+
+    def attack(self):
+        print(f'attack with {self.arrows} arrows')
+
+
+wizard1 = Wizard('pepe', 20)
+archer1 = Archer('paco', 40)
